f9dac73924da7c2f996e65aed5a4693f9b5deb5e
Anatolii Sakhnik <sakhnik@gmail.com>
2021-08-17 20:37:26 +0300

    Add typing to the method parameters, fix warnings

--- copy_younger/__init__.py
+++ copy_younger/__init__.py
@@ -1,11 +1,15 @@
 """."""
 
-from anki.notes import NoteId
+from anki.collection import OpChanges
+from anki.models import NotetypeId
+from anki.notes import Note, NoteId
 import aqt
-from aqt import mw
+from aqt import mw, Collection
+from aqt.browser import Browser
 from aqt.operations import CollectionOp
 from aqt.utils import tooltip, shortcut
 from aqt.qt import QAction
+import typing
 from typing import List
 
 
@@ -36,12 +40,14 @@ def determine_child_index(card_type: str) -> int:
     return -1
 
 
-def copy_note(col: aqt.Collection, nid: NoteId):
+def copy_note(col: Collection, nid: NoteId):
     """."""
     note = col.get_note(nid)
     model = note.note_type()
     # Let's assume all the cards belong to the same deck for now
     src_deck = col.decks.get(note.cards()[0].did)
+    if not src_deck or not model:
+        return
     src_deck_name = src_deck["name"]
 
     # Source card type
@@ -52,11 +58,11 @@ def copy_note(col: aqt.Collection, nid: NoteId):
     if src_index == -1:
         word = note["Front"]
         tooltip(f"Not one of {','.join(names)}: {word}", period=2000)
-        return []
+        return
     if src_index > len(names) - 2:
         word = note["Front"]
         tooltip(f"Can't copy from {names[-1]}: {word}", period=2000)
-        return []
+        return
 
     src_name = names[src_index]
 
@@ -67,11 +73,21 @@ def copy_note(col: aqt.Collection, nid: NoteId):
     if dst_tag in note.tags:
         word = note["Front"]
         tooltip(f"Already copied: {word}", period=2000)
-        return []
+        return
 
     # Assign model to deck
-    dst_deck = col.decks.by_name(src_deck_name.replace(src_name, dst_name))
-    dst_type = col.models.by_name(src_type.replace(src_name, dst_name))
+    dst_deck_name = src_deck_name.replace(src_name, dst_name)
+    dst_deck = col.decks.by_name(dst_deck_name)
+    if not dst_deck:
+        tooltip(f"No such deck {dst_deck_name}", period=2000)
+        return
+
+    dst_type_name = src_type.replace(src_name, dst_name)
+    dst_type = col.models.by_name(dst_type_name)
+    if not dst_type:
+        tooltip(f"No such note type {dst_type_name}", period=2000)
+        return
+
     col.decks.select(dst_deck['id'])
 
     new_note = col.new_note(dst_type)
@@ -85,12 +101,12 @@ def copy_note(col: aqt.Collection, nid: NoteId):
     col.update_note(note)
 
 
-def copy_for_younger(col: aqt.Collection, browser):
+def copy_for_younger(col: Collection, browser: Browser) -> OpChanges:
     """."""
     # Set checkpoint
-    pos = col.add_custom_undo_entry("copy younger")
+    pos = col.add_custom_undo_entry("Copy younger")
 
-    nids = browser.selectedNotes()
+    nids = browser.selected_notes()
     if not nids:
         tooltip("No notes selected.", period=2000)
         return col.merge_undo_entries(pos)
@@ -102,20 +118,25 @@ def copy_for_younger(col: aqt.Collection, browser):
     return col.merge_undo_entries(pos)
 
 
-def copy_op(*, parent: aqt.QWidget, browser):
+def copy_op(*, parent: aqt.QWidget, browser: Browser) -> CollectionOp:
     return CollectionOp(parent, lambda col: copy_for_younger(col, browser))
 
 
-def synchronize_child(col, child_name, src_mid, dst_mid):
+def synchronize_child(col: Collection, child_name: str,
+                      src_mid: NotetypeId, dst_mid: NotetypeId) -> List[Note]:
     """Synchronize all the notes of the model dst_mid taking data
     from the model src_mid."""
-    src_type_name = col.models.get(src_mid)['name']
-    dst_type_name = col.models.get(dst_mid)['name']
+    src_type = col.models.get(src_mid)
+    assert src_type
+    src_type_name = src_type['name']
+    dst_type = col.models.get(dst_mid)
+    assert dst_type
+    dst_type_name = dst_type['name']
     print(f"Synchronizing {dst_type_name}")
     notes_to_flush = []
     # Iterate over all the notes of the destination model
     for nid in col.models.nids(dst_mid):
-        note = col.getNote(nid)
+        note = col.get_note(nid)
         # Use the field "front" as the search key to find the source note
         front_field = note.fields[0]
         query = f'note:"{src_type_name}" front:"{front_field}"'
@@ -124,7 +145,7 @@ def synchronize_child(col, child_name, src_mid, dst_mid):
             print(f"Can't find the source of {front_field}")
             continue
         # There's the source note
-        src_note = col.getNote(src_nids[0])
+        src_note = col.get_note(src_nids[0])
         changed = False
         # Just overwrite the fields if different
         if src_note.fields != note.fields:
@@ -147,7 +168,7 @@ def synchronize_child(col, child_name, src_mid, dst_mid):
     return notes_to_flush
 
 
-def synchronize_younger(col):
+def synchronize_younger(col: Collection) -> OpChanges:
     """Synchronize the notes assuming the name[0] is the master copy.
 
     When notes evolve, fields and tags may change. We'll iterate over
@@ -157,13 +178,14 @@ def synchronize_younger(col):
     """
 
     # Set checkpoint
-    pos = col.add_custom_undo_entry("synchronize child")
+    pos = col.add_custom_undo_entry("Synchronize child")
     notes_to_flush = []
 
     # Iterate over all the models
     for mid_name in col.models.all_names_and_ids():
         # Only English requires synchronization for now
         mname = mid_name.name
+        mid = typing.cast(NotetypeId, mid_name.id)
         if "English" not in mname:
             continue
         child_index = determine_child_index(mname)
@@ -171,15 +193,16 @@ def synchronize_younger(col):
             child_name = names[child_index]
             src_mname = mname.replace(child_name, names[0])
             src_mid = col.models.id_for_name(src_mname)
-            notes_to_flush += synchronize_child(col, child_name, src_mid,
-                                                mid_name.id)
+            if not src_mid:
+                continue
+            notes_to_flush += synchronize_child(col, child_name, src_mid, mid)
 
     if notes_to_flush:
         col.update_notes(notes_to_flush)
     return col.merge_undo_entries(pos)
 
 
-def sync_op(*, parent: aqt.QWidget):
+def sync_op(*, parent: aqt.QWidget) -> CollectionOp:
     return CollectionOp(parent, lambda col: synchronize_younger(col))
 
 
